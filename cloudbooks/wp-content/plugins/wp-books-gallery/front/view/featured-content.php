d548def5e959647dbe187dd6b99a29ae19628a4e
Isaque Alcantara <isaque21@gmail.com>
2023-10-30 22:08:00 -0300

    alteracao no metodo de importar o site

--- cloudbooks/wp-content/plugins/wp-books-gallery/front/view/featured-content.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-# Silence is golden.
-if ( !defined( 'ABSPATH' ) ) {
-    exit;
-}
--- cloudbooks/wp-content/themes/envo-ecommerce/page.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-
-<?php get_template_part( 'template-parts/template-part', 'head' ); ?>
-
-<!-- start content container -->
-<?php get_template_part( 'content', 'page' ); ?>
-<!-- end content container -->
-
-<?php get_footer(); ?>
